6dd5890aaa49f8472499a0b6ebfbf66fd4785f99
Eug4n4 <Eug4n4@users.noreply.github.com>
2026-03-26 11:49:50 +0200

    fix: enableCors instead of library function

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import { GlobalExceptionFilter } from "./modules/shared/exception.filter";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { rawBody: true });
     app.setGlobalPrefix("uevent/v1");
-    app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+    app.enableCors({ origin: process.env.FRONTEND_URL, credentials: true })
     app.use(cookieParser());
     app.useGlobalFilters(new GlobalExceptionFilter());
     app.useGlobalPipes(
